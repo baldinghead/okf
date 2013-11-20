1a3c5b54f68feffbba6a85ccd211beedf13749eb
福田芳真 <yoshima@yoshima.local>
2013-11-20 23:29:44 +0900

    edit controller for connect elasticsearch
    とりあえずrestでアクセスしてログに全件だしてみた．

--- app/controllers/sample_elasticsearches_controller.rb
+++ app/controllers/sample_elasticsearches_controller.rb
@@ -1,4 +1,15 @@
+require "open-uri"
+
 class SampleElasticsearchesController < ApplicationController
+  def initialize
+    @request_url = "http://59.106.182.98:9200/daily/page/_search?pretty"
+  end
+
   def show
+    logger.debug("start")
+    result = open(@request_url)
+    json_object = JSON.load(result)
+    logger.debug(json_object)
+    logger.debug("end")
   end
 end
