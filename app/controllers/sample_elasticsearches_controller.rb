6e456dc91dd59737df7443b82b247dc2000c80d6
福田芳真 <yoshima@yoshima.local>
2013-11-23 22:10:19 +0900

    after edit parse json object
    elasticsearchから必要な情報（title, link, auth）だけ取得して，そのオブジェクトの中のhitsだけを取り出す処理を追加．

--- app/controllers/sample_elasticsearches_controller.rb
+++ app/controllers/sample_elasticsearches_controller.rb
@@ -2,14 +2,15 @@ require "open-uri"
 
 class SampleElasticsearchesController < ApplicationController
   def initialize
-    @request_url = "http://59.106.182.98:9200/daily/page/_search?pretty"
+    @request_url = "http://59.106.182.98:9200/daily/page/_search?pretty&fields=link,title,author"
   end
 
   def show
     logger.debug("start")
     result = open(@request_url)
     json_object = JSON.load(result)
-    logger.debug(json_object)
+    @hits = json_object["hits"]["hits"]
+    logger.debug(@hits)
     logger.debug("end")
   end
 end
