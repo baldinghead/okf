946f2bef2c0e5ee89903f568e20f8f835141a054
yfujihara <hoge@hoge.com>
2014-01-20 08:38:40 +0900

    deployコントローラのCSRF対応を無効化

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -1,12 +1,17 @@
 class DeployController < ApplicationController
   
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
+    
   def index
+    
     
     here = File.dirname(__FILE__)
     scriptPath = here + "/../../build.sh"
 
     logger.info(scriptPath)
     system(scriptPath)
+    
     
     render :nothing => true
   end
