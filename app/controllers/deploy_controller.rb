0e0322bc07effe329a871afc762929b9e0c44e79
yfujihara <hoge@hoge.com>
2014-01-26 17:22:27 +0900

    idobata auto comment

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -6,8 +6,19 @@ class DeployController < ApplicationController
   def index
     here = File.dirname(__FILE__)
     scriptPath = here + "/../../build.sh"
-
-    logger.info(scriptPath)
+    
+    logger.info(params['commits'][0])
+    author = params['commits'][0]['author']
+    branch = params['commits'][0]['branch']
+    message = author + '  ' + branch + "<br />" + params['commits'][0]['message']
+    
+    logger.info("message=" + message)
+    
+    curlCmd = 'curl --data-urlencode "source=' + message + '" -d format=html https://idobata.io/hook/4a11714e-6b1a-41fb-9cd3-3f533ce70e92'
+    logger.info(curlCmd)
+    system(curlCmd)
+    #logger.info(params)
+    #デプロイシェルを実行
     system(scriptPath)
     
     render :nothing => true
