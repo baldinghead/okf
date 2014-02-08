ffe87b097b0c447db1eb12f41c5976a626049f21
福田芳真 <yoshima@yoshima.local>
2014-02-08 10:54:41 +0900

    edit 404
    404pageを動的に生成するように変更．

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from ActionController::RoutingError, :with => :render_404
- def render_404(exception = nil)
+  def render_404(exception = nil)
 
-render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false, :content_type => 'text/html'
+  #render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false, :content_type => 'text/html'
+    render :template => "errors/error_404", :status => 404, :layout => 'application', :content_type => 'text/html'
   end
 end
